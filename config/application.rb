02a83c47fa1f8d18e2f25bf5a509d7a5ecc86dfa
jordimairena <jordim95@hotmail.com>
2018-04-05 23:57:05 -0600

    backend

--- config/application.rb
+++ config/application.rb
@@ -29,10 +29,10 @@ module ClimaApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
